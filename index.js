c99a2334ed3f2cd6794ffed8a797e01cd5af1391
Siva Kumar <skumar244@sapient.com>
2017-03-25 09:32:05 +0000

    fb cards list

--- index.js
+++ index.js
@@ -48,7 +48,8 @@ restService.post('/echo', function (req, res) {
                 return res.json({
                     speech: "Here is your order history",
                     source: 'webhook-echo-one',
-                    "messages": fnProductList(JSON.parse(response).products)
+                    // "messages": thumbnails(JSON.parse(response).products)
+                    "message": thumbnails()
                 });
             });
             break;
@@ -138,6 +139,141 @@ restService.post('/fb-test', function (req, res) {
 restService.get('/', function (req, res) {
     res.send('hello world');
 })
+
+
+function thumbnails() {
+    return [
+        {
+            "type": 0,
+            "speech": ""
+        },
+        {
+            "payload": {
+                "facebook": {
+                    "attachment": {
+                        "type": "template",
+                        "payload": {
+                            "template_type": "list",
+                            "elements": [
+                                {
+                                    "title": "Classic T-Shirt Collection",
+                                    "image_url": "http://asset1.marksandspencer.com/is/image/mands/SD_01_T38_7237_KC_X_EC_0?$PRODVIEWER_SUB$",
+                                    "subtitle": "See all our colors",
+                                    "default_action": {
+                                        "type": "web_url",
+                                        "url": "https://peterssendreceiveapp.ngrok.io/shop_collection",
+                                        "messenger_extensions": true,
+                                        "webview_height_ratio": "tall",
+                                        "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                                    },
+                                    "buttons": [
+                                        {
+                                            "title": "View",
+                                            "type": "web_url",
+                                            "url": "https://peterssendreceiveapp.ngrok.io/collection",
+                                            "messenger_extensions": true,
+                                            "webview_height_ratio": "tall",
+                                            "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                                        }
+                                    ]
+                                },
+                                {
+                                    "title": "Classic White T-Shirt",
+                                    "image_url": "http://asset1.marksandspencer.com/is/image/mands/SD_01_T38_7237_KC_X_EC_0?$PRODVIEWER_SUB$",
+                                    "subtitle": "100% Cotton, 200% Comfortable",
+                                    "default_action": {
+                                        "type": "web_url",
+                                        "url": "https://peterssendreceiveapp.ngrok.io/view?item=100",
+                                        "messenger_extensions": true,
+                                        "webview_height_ratio": "tall",
+                                        "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                                    },
+                                    "buttons": [
+                                        {
+                                            "title": "Shop Now",
+                                            "type": "web_url",
+                                            "url": "https://peterssendreceiveapp.ngrok.io/shop?item=100",
+                                            "messenger_extensions": true,
+                                            "webview_height_ratio": "tall",
+                                            "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                                        }
+                                    ]
+                                },
+                                {
+                                    "title": "Classic Blue T-Shirt",
+                                    "image_url": "http://asset1.marksandspencer.com/is/image/mands/SD_01_T38_7237_KC_X_EC_0?$PRODVIEWER_SUB$",
+                                    "subtitle": "100% Cotton, 200% Comfortable",
+                                    "default_action": {
+                                        "type": "web_url",
+                                        "url": "https://peterssendreceiveapp.ngrok.io/view?item=101",
+                                        "messenger_extensions": true,
+                                        "webview_height_ratio": "tall",
+                                        "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                                    },
+                                    "buttons": [
+                                        {
+                                            "title": "Shop Now",
+                                            "type": "web_url",
+                                            "url": "https://peterssendreceiveapp.ngrok.io/shop?item=101",
+                                            "messenger_extensions": true,
+                                            "webview_height_ratio": "tall",
+                                            "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                                        }
+                                    ]
+                                },
+                                {
+                                    "title": "Classic Black T-Shirt",
+                                    "image_url": "http://asset1.marksandspencer.com/is/image/mands/SD_01_T38_7237_KC_X_EC_0?$PRODVIEWER_SUB$",
+                                    "subtitle": "100% Cotton, 200% Comfortable",
+                                    "default_action": {
+                                        "type": "web_url",
+                                        "url": "https://peterssendreceiveapp.ngrok.io/view?item=102",
+                                        "messenger_extensions": true,
+                                        "webview_height_ratio": "tall",
+                                        "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                                    },
+                                    "buttons": [
+                                        {
+                                            "title": "Shop Now",
+                                            "type": "web_url",
+                                            "url": "https://peterssendreceiveapp.ngrok.io/shop?item=102",
+                                            "messenger_extensions": true,
+                                            "webview_height_ratio": "tall",
+                                            "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                                        }
+                                    ]
+                                }
+                            ],
+                            "buttons": [
+                                {
+                                    "title": "View More",
+                                    "type": "postback",
+                                    "payload": "payload"
+                                }
+                            ]
+                        }
+                    }
+                },
+                "kik": {
+                    "type": "",
+                    "body": ""
+                },
+                "slack": {
+                    "text": "",
+                    "attachments": []
+                },
+                "telegram": {
+                    "text": ""
+                },
+                "viber": {
+                    "type": "text",
+                    "text": ""
+                }
+            },
+            "type": 4
+        }
+    ]
+}
 
 
 function fnProductList(productData) {
